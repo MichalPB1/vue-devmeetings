7562925e955b6018b81c0e2187373ef9bfae8b59
Michal_PB1 <kontakt@michalpb1.pl>
2018-09-08 13:51:55 +0200

    remove search close

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,6 +2,7 @@
   <div class="products">
     <div class="products-search">
       <input type="text" v-model="search" placeholder="Search a product...">
+      <span v-show="search.length" class="products-search_clear" @click="clearSearch">×</span>
     </div>
     <div class="products" v-if="filteredList && filteredList.length">
         <product-list-item :offer="product" v-for="(product, index) in filteredList" :key="index"  @remove="removeProduct(...arguments)"></product-list-item>
@@ -34,6 +35,9 @@ export default {
     }
   },
   methods: {
+    clearSearch() {
+      this.search = ''
+    },
     removeProduct(id) {
       this.$emit('remove', id)
     }
@@ -58,10 +62,19 @@ export default {
 }
 .products-search {
   margin-bottom: 30px;
+  position: relative;
 }
 .products-search input {
   padding: 20px;
   border: 1px solid #e1e1e1;
+  padding-right: 50px;
+}
+.products-search_clear {
+  position: absolute;
+  right: 25px;
+  top: 20px;
+  font-size: 20px;
+  cursor: pointer;
 }
 .error {
   color: red;
