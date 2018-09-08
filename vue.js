b64b19be528a0d19371621b257962effbe274ecd
Michal_PB1 <kontakt@michalpb1.pl>
2018-09-08 10:18:05 +0200

    [#1.1]

---
+++ vue.js
@@ -0,0 +1,20 @@
+new Vue({
+  el: '#app',
+  data: {
+    test: 'Dev Meetings',
+    products: [
+      {
+        id: 1,
+        name: 'Product 1'
+      },
+      {
+        id: 2,
+        name: 'Product 2'
+      },
+      {
+        id: 3,
+        name: 'Product 3'
+      }
+    ]
+  }
+})
